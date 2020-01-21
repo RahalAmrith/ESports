d6c589068a5f9e0a281ac78d4a5eddf6060da0b0
Yunly International <yunlyinternational@gmail.com>
2020-01-21 15:34:13 +0530

    fixed serve error

--- Frontend/src/view/common/navbar.js
+++ Frontend/src/view/common/navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import "../assets/common/navbar.css";
+import '../../App.css'
 
 import Logo from "../images/navbar/logo.png";
 
