441ebd06790d951382ff45156d6449cb44a004b6
RahalAmrith <rahalamrith46@gmail.com>
2020-04-27 19:45:23 +0530

    changed landing

--- Frontend/src/view/home/gameCard.js
+++ Frontend/src/view/home/gameCard.js
@@ -13,33 +13,16 @@ class gameCard extends Component {
 
   render() {
     return (
-      <div className="col-md-2">
-        <Link to={this.props.link}>
-          <div
-            // ref={ele => {
-            //   ele === null
-            //     ? console.log()
-            //     : (ele.style.height = ele.offsetWidth * 0.5625 + "px");
-            // }}
-            className="home_game_card"
-            style={{
-              backgroundImage: 'url("' + this.props.img + '")',
-              height:
-                this.element === null
-                  ? `100px`
-                  : `${this.element.innerWidth * 0.5625} px`
-            }}
-          >
-            <div
-              className="home_game_card_content"
-              onMouseOver={() => this.props.setBanner(this.props.bannerNum)}
-              onMouseLeave={() => this.props.setBanner(0)}
-            >
-              <h1>{this.props.title}</h1>
-            </div>
-          </div>
-        </Link>
-      </div>
+      <Link className="home_game_link" to={this.props.link}>
+        <div
+          className="home_game_card"
+          onMouseOver={() => this.props.setBanner(this.props.bannerNum)}
+          onMouseLeave={() => this.props.setBanner(0)}
+        >
+          <img src={this.props.img} alt="" />
+          <h1>{this.props.title}</h1>
+        </div>
+      </Link>
     );
   }
 }
--- Frontend/src/view/home/home.js
+++ Frontend/src/view/home/home.js
@@ -9,12 +9,12 @@ import DOTA2 from "../images/home/dota2_esports_earnings.jpg";
 import CSGO from "../images/home/csgo_esportsearnings.jpg";
 import OverWatch from "../images/home/overwatch_esportsearnings.jpg";
 import LOL from "../images/home/leagueoflegends_esportsearnings.jpg";
-import Banner_ALl from "../images/home/Banner/All.jpg";
-import Banner_PUBG from "../images/home/Banner/PUBG.jpg";
-import Banner_DOTA2 from "../images/home/Banner/DOTA2.jpg";
-import Banner_CSGO from "../images/home/Banner/CSGO.jpg";
-import Banner_OverWatch from "../images/home/Banner/OverWatch.jpg";
-import Banner_LOL from "../images/home/Banner/LOL.jpg";
+// import Banner_ALl from "../images/home/Banner/All.jpg";
+// import Banner_PUBG from "../images/home/Banner/PUBG.jpg";
+// import Banner_DOTA2 from "../images/home/Banner/DOTA2.jpg";
+// import Banner_CSGO from "../images/home/Banner/CSGO.jpg";
+// import Banner_OverWatch from "../images/home/Banner/OverWatch.jpg";
+// import Banner_LOL from "../images/home/Banner/LOL.jpg";
 
 // Components
 import GameCard from "./gameCard.js";
@@ -38,15 +38,24 @@ class Home extends Component {
       recentTournemants: [],
       topPlayers: [],
       recentPostList: [],
-      bannerImgs: [
-        Banner_ALl,
-        Banner_DOTA2,
-        Banner_CSGO,
-        Banner_OverWatch,
-        Banner_LOL,
-        Banner_PUBG
+      bannerImgs: [PUBG, DOTA2, CSGO, OverWatch, LOL],
+      bannerTitles: ["PUBG", "DOTA2", "CS:GO", "Over Watch", "LOL"],
+      bannerLinks: [
+        "/games/pubg",
+        "/games/dota2",
+        "/games/csgo",
+        "/games/overwatch",
+        "/games/lol",
       ],
-      currentBannerImg: 0
+      currentBannerImg: 0,
+
+      bannerDesc: [
+        "PlayerUnknown's Battlegrounds (PUBG) is an online multiplayer battle royale game developed and published by PUBG Corporation, a subsidiary of South Korean video game company Bluehole.",
+        "Dota 2 is a multiplayer online battle arena (MOBA) video game developed and published by Valve. The game is a sequel to Defense of the Ancients (DotA), which was a community-created mod for Blizzard Entertainment's Warcraft III: Reign of Chaos and its expansion pack, The Frozen Throne.",
+        "Counter-Strike: Global Offensive (CS:GO) is a multiplayer first-person shooter developed by Valve and Hidden Path Entertainment. It is the fourth game in the Counter-Strike series and was released for Windows, OS X, Xbox 360, and PlayStation 3 in August 2012, while the Linux version was released in 2014.",
+        "Overwatch is a team-based multiplayer first-person shooter developed and published by Blizzard Entertainment. Described as a 'hero shooter', Overwatch assigns players into two teams of six, with each player selecting from a roster of over 30 characters, known as 'heroes', each with a unique style of play that is divided into three general roles that fit their purpose.",
+        "League of Legends (LoL) is a multiplayer online battle arena video game developed and published by Riot Games for Microsoft Windows and macOS. Inspired by the Warcraft III: The Frozen Throne mod Defense of the Ancients, the game follows a freemium model and is supported by microtransactions.",
+      ],
     };
   }
 
@@ -55,7 +64,7 @@ class Home extends Component {
     var _playersList = await _Players.DB_list(18);
 
     await this.setState({
-      topPlayers: _playersList
+      topPlayers: _playersList,
     });
 
     // get recent tournemants
@@ -75,7 +84,7 @@ class Home extends Component {
     });
 
     await this.setState({
-      recentTournemants: _finalTournemantsList
+      recentTournemants: _finalTournemantsList,
     });
 
     // get resent posts
@@ -83,13 +92,13 @@ class Home extends Component {
     var _recentPosts = await Blog.getPostList();
 
     await this.setState({
-      recentPostList: _recentPosts
+      recentPostList: _recentPosts,
     });
   }
 
   setBannerImage(num) {
     this.setState({
-      currentBannerImg: num
+      currentBannerImg: num,
     });
   }
 
@@ -110,25 +119,25 @@ class Home extends Component {
             slidesToShow: 3,
             slidesToScroll: 3,
             infinite: true,
-            dots: true
-          }
+            dots: true,
+          },
         },
         {
           breakpoint: 600,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
-            initialSlide: 2
-          }
+            initialSlide: 2,
+          },
         },
         {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
+            slidesToScroll: 1,
+          },
+        },
+      ],
     };
 
     const _posts = this.state.recentPostList.map((data, i) => {
@@ -139,10 +148,11 @@ class Home extends Component {
       return (
         <h5 key={i}>
           <img
-              className="flag"
-              title="" alt="Esports earnings  "
-              src={Config.parseCountry(data.country).flag}
-            />
+            className="flag"
+            title=""
+            alt="Esports earnings  "
+            src={Config.parseCountry(data.country).flag}
+          />
           {data.player_name}
           <span style={{ float: "right", color: "#00ff00" }}>
             {numeral(data.total_earning).format("$ 0.00 a")}
@@ -153,153 +163,91 @@ class Home extends Component {
 
     return (
       <div className="container-fluid row mb-5">
-        <div className="col-md-3 mb-5">
-          <div className="home_container">
-            <h1>Top Players</h1>
-            {topPlayersList}
-          </div>
-        </div>
-        <div className="col-md-6 mb-5">
-          <div className="home_container">
-            <h1>Top 5 Esports Earning Games</h1>
-            <div className="home_banner_back">
+        {/* ============================== */}
+        {/* ========== left col ========== */}
+        {/* ============================== */}
+        <div className="col-sm-8">
+          {/* gameCards */}
+          <div className="home_newBanner home_container">
+            <div className="bigBanner">
               <img
-                className="home_banner"
-                src={this.state.bannerImgs[0]}
-                style={
-                  this.state.currentBannerImg === 0
-                    ? { opacity: 1, transform: "translateY(0px)" }
-                    : { opacity: 0, transform: "translateY(-20.23vw)" }
-                }
-                title="" alt="Esports earnings  WorlEsports Earnnings Games"
+                src={this.state.bannerImgs[this.state.currentBannerImg]}
+                alt=""
               />
-              <img
-                className="home_banner"
-                src={this.state.bannerImgs[1]}
-                style={
-                  this.state.currentBannerImg === 1
-                    ? { opacity: 1, transform: "translateY(-20.23vw)" }
-                    : { opacity: 1, transform: "translateY(0px)" }
-                }
-                title="" alt="Esports earnings  WorlEsports Earnnings Games"
-              />
-              <img
-                className="home_banner"
-                src={this.state.bannerImgs[2]}
-                style={
-                  this.state.currentBannerImg === 2
-                    ? { opacity: 1, transform: "translateY(-40.47vw)" }
-                    : { opacity: 0, transform: "translateY(-20.23vw)" }
-                }
-                title="" alt="Esports earnings  WorlEsports Earnnings Games"
-              />
-              <img
-                className="home_banner"
-                src={this.state.bannerImgs[3]}
-                style={
-                  this.state.currentBannerImg === 3
-                    ? { opacity: 1, transform: "translateY(-60.71vw)" }
-                    : { opacity: 0, transform: "translateY(-40.47vw)" }
-                }
-                title="" alt="Esports earnings  WorlEsports Earnnings Games"
-              />
-              <img
-                className="home_banner"
-                src={this.state.bannerImgs[4]}
-                style={
-                  this.state.currentBannerImg === 4
-                    ? { opacity: 1, transform: "translateY(-80.94vw)" }
-                    : { opacity: 0, transform: "translateY(-60.71vw)" }
-                }
-                title="" alt="Esports earnings  WorlEsports Earnnings Games"
-              />
-              <img
-                className="home_banner"
-                src={this.state.bannerImgs[5]}
-                style={
-                  this.state.currentBannerImg === 5
-                    ? { opacity: 1, transform: "translateY(-101.18vw)" }
-                    : { opacity: 0, transform: "translateY(-80.94vw)" }
-                }
-                title="" alt="Esports earnings  WorlEsports Earnnings Games"
-              />
-            </div>
 
-            {/* Games */}
-            <div className="row">
-              <div className="col-md-1"></div>
+              <h1>{this.state.bannerTitles[this.state.currentBannerImg]}</h1>
+
+              <p>{this.state.bannerDesc[this.state.currentBannerImg]}</p>
+            </div>
+            <div className="tiles">
+              <GameCard
+                bannerNum={0}
+                setBanner={this.setBannerImage.bind(this)}
+                img={this.state.bannerImgs[0]}
+                title={this.state.bannerTitles[0]}
+                link={this.state.bannerLinks[0]}
+              />
               <GameCard
                 bannerNum={1}
                 setBanner={this.setBannerImage.bind(this)}
-                img={DOTA2}
-                title="DOTA 2"
-                link="/games/dota2"
+                img={this.state.bannerImgs[1]}
+                title={this.state.bannerTitles[1]}
+                link={this.state.bannerLinks[1]}
               />
               <GameCard
                 bannerNum={2}
                 setBanner={this.setBannerImage.bind(this)}
-                img={CSGO}
-                title="CS:GO"
-                link="games/csgo"
+                img={this.state.bannerImgs[2]}
+                title={this.state.bannerTitles[2]}
+                link={this.state.bannerLinks[2]}
               />
               <GameCard
                 bannerNum={3}
                 setBanner={this.setBannerImage.bind(this)}
-                img={OverWatch}
-                title="OVERWATCH"
-                link="/games/overwatch"
+                img={this.state.bannerImgs[3]}
+                title={this.state.bannerTitles[3]}
+                link={this.state.bannerLinks[3]}
               />
               <GameCard
                 bannerNum={4}
                 setBanner={this.setBannerImage.bind(this)}
-                img={LOL}
-                title="LOL"
-                link="/games/lol"
+                img={this.state.bannerImgs[4]}
+                title={this.state.bannerTitles[4]}
+                link={this.state.bannerLinks[4]}
               />
-              <GameCard
-                bannerNum={5}
-                setBanner={this.setBannerImage.bind(this)}
-                img={PUBG}
-                title="PUBG"
-                link="/games/pubg"
-              />
-              <div className="col-md-1"></div>
             </div>
+          </div>
+
+          {/* recent Tournemants */}
+          <div className="home_container mb-5">
+            <h1>Recent Tournemants and Prize Pools</h1>
+            {/* <hr /> */}
+
+            <TournemantContainer data={this.state.recentTournemants} />
+          </div>
+
+          {/* Upcomming Leagus */}
+          <div className="home_container mb-5">
+            <h1>Recent Posts</h1>
+            <hr />
+            <Slider {...settings}>{_posts}</Slider>
           </div>
         </div>
 
-        <div className="col-md-3 mb-5">
+        {/* =============================== */}
+        {/* ========== right col ========== */}
+        {/* =============================== */}
+        <div className="col-sm-4">
+          {/* Top Players */}
+          <div className="home_container">
+            <h1>Top Players</h1>
+            {topPlayersList}
+          </div>
+
+          {/* advertiesmen */}
           <div className="homeAdvertiesment">
             <h4>Contact us for advertise here</h4>
           </div>
-        </div>
-
-        <div className="col-12 home_header">
-          <center>
-            <h1>Welcome to eSports </h1>
-
-            <p>
-              Esports is a form of sport competition using video games. Esports
-              often takes the form of organized, multiplayer video game
-              competitions, particularly between professional players,
-              individually or as teams.
-            </p>
-          </center>
-        </div>
-
-        {/* recent Tournemants */}
-        <div className="container home_container mb-5">
-          <h1>Recent Tournemants and Prize Pools</h1>
-          {/* <hr /> */}
-
-          <TournemantContainer data={this.state.recentTournemants} />
-        </div>
-
-        {/* Upcomming Leagus */}
-        <div className="container home_container mb-5">
-          <h1>Recent Posts</h1>
-          <hr />
-          <Slider {...settings}>{_posts}</Slider>
         </div>
       </div>
     );
